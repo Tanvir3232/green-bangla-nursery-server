dd5793dd404eecfb1adac0c61c0232af333ad2c7
Tanvir3232 <tanvirahmadstudent@gmail.com>
2024-07-09 23:09:31 +0600

    implement global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express, { Application, Request, Response } from 'express';
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import router from "./app/routes";
 
 
@@ -16,5 +17,5 @@ app.use('/api/', router);
 app.get('/', (req: Request, res: Response) => {
     res.json('Welcome to Green Bangla Nursery');
 });
-
+app.use(globalErrorHandler)
 export default app;
